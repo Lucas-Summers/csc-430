a2d258225a01187df4c3638ac24166f163c12a21
Lucas Summers <summerslucas64@gmail.com>
2024-10-24 09:59:55 -0700

    finished asgn4, needs tests

--- asgn4.rkt
+++ asgn4.rkt
@@ -3,8 +3,9 @@
 #lang typed/racket
 (require typed/rackunit)
 
-; defines a lexical environment as a list of bindings
+; defines a lexical environment
 (define-type Env (Listof Binding))
+
 ; defines an environment binding
 (struct Binding ([name : Symbol] [val : Value]) #:transparent)
 (define mt-env '()) ; empty environment
@@ -12,103 +13,111 @@
 
 ; defines a value type
 (define-type Value (U NumV BoolV StringV PrimV ClosV))
+
 ; defines a number value
 (struct NumV ([n : Real]) #:transparent)
-; defines a boolean value
-(struct BoolV ([b : Boolean]) #:transparent)
 ; defines a string value
 (struct StringV ([s : String]) #:transparent)
-; supports both unary (only error) and binary operators
+; defines a boolean value
+(struct BoolV ([b : Boolean]) #:transparent)
 ; defines a primitive operator value
-; NOTE: using struct instead of define-type gives access to PrimV? predicate
-(struct PrimV () #:transparent)
-; defines binary primitive operators (+, -, *, /, equal?, <=)
-(struct BinaryP PrimV ([op : (-> Value Value Value)]) #:transparent)
-; defines unary primitive operators (just error for now)
-(struct UnaryP PrimV ([op : (-> Value Value)]) #:transparent)
+(struct PrimV ([op : Symbol]) #:transparent)
 ; defines a closure
-(struct ClosV ([args : (Listof Symbol)] [body : ExprC] [env : Env]))
+(struct ClosV ([args : (Listof Symbol)] [body : ExprC] [env : Env]) #:transparent)
 
 ; defines an expression type
-(define-type ExprC (U Value IdC AppC LamC)) ; remove Value?
+(define-type ExprC (U Value IdC AppC LamC IfC)) ; remove Value?
+
 ; defines an identifier
 (struct IdC ([s : Symbol]) #:transparent)
 ; defines a function application
 (struct AppC ([fun : ExprC] [args : (Listof ExprC)]) #:transparent)
 ; defines a lambda function
-(struct LamC ([args : (Listof Symbol)] [body : ExprC]))
-
-(define (binop [s : Symbol] [l : Value] [r : Value]) : Value
-  (cond
-    [(and (NumV? l) (NumV? r)) 
-      (match s
-        ['+ (NumV (+ (NumV-n l) (NumV-n r)))]
-        ['- (NumV (- (NumV-n l) (NumV-n r)))]
-        ['* (NumV (* (NumV-n l) (NumV-n r)))]
-        ['/ (NumV (/ (NumV-n l) (NumV-n r)))]
-        ['<= (BoolV (<= (NumV-n l) (NumV-n r)))])]
-    [(symbol=? s 'equal?) (if (or (or (PrimV? l) (ClosV? l)) (or (PrimV? r) (ClosV? r))) ;confused here - muzart
-                              (error 'arith "[AAQZ]: cannot compare closures or primitives")
-                              (BoolV (equal? l r)))]
-    [else (error 'arith "[AAQZ] at least on argument was not a number")]))
+(struct LamC ([args : (Listof Symbol)] [body : ExprC]) #:transparent)
+; defines an if statement
+(struct IfC ([test : ExprC] [then : ExprC] [else : ExprC]) #:transparent)
 
 ; the top-level environment installed with primitive operations and true/false
 (define top-env
   (list
-   (Binding 'false (BoolV false))
-   (Binding 'true (BoolV true))
-   (Binding '+ (BinaryP (lambda ([a : Value] [b : Value]) : Value (binop '+ a b))))
-   (Binding '* (BinaryP (lambda ([a : Value] [b : Value]) : Value (binop '* a b))))
-   (Binding '- (BinaryP (lambda ([a : Value] [b : Value]) : Value (binop '- a b))))
-   (Binding '/ (BinaryP (lambda ([a : Value] [b : Value]) : Value (binop '/ a b))))
-   (Binding '<= (BinaryP (lambda ([a : Value] [b : Value]) : Value (binop '<= a b))))
-   (Binding 'equal? (BinaryP (lambda ([a : Value] [b : Value]) : Value (binop 'equal? a b))))
-   (Binding 'error (UnaryP (lambda ([a : Value]) (error "[AAQZ] user-error: ~e" (serialize a)))))))
+   (Binding 'false (PrimV 'false))
+   (Binding 'true (PrimV 'true))
+   (Binding '+ (PrimV '+))
+   (Binding '* (PrimV '*))
+   (Binding '- (PrimV '-))
+   (Binding '/ (PrimV '/))
+   (Binding '<= (PrimV '<=))
+   (Binding 'equal? (PrimV 'equal?))
+   (Binding 'error (PrimV 'error))))
 
 ; given an s-expression, combine parsing and evaluation to produced a result
-;(define (top-interp [s : Sexp]) : String
-;  (serialize (interp (parse s) top-env)))
+(define (top-interp [s : Sexp]) : String
+  (serialize (interp (parse s) top-env)))
 
-; TODO parse to the new setup
-; parse an s-expression into an ExprC
-;(define (parse [s : Sexp]) : ExprC
-;  (match s
-;    [(? real? n) (NumV n)]
-;    [(? symbol? s) (IdC (valid-id? s))]
-;    [(list (? symbol? f) a ...) (AppC (valid-id? f) (map parse a))]
-;    [other (error 'parse "[AAQZ] syntax error: ~e" other)]))
-#;(define (parse [s : Sexp]) : ExprC
+(define (parse [s : Sexp]) : ExprC
   (match s
-    ; Numbers to NumV
     [(? real? n) (NumV n)]
-    
-    ; Strings to StringV
-    [(? string? str) (StringV str)]
-    
-    ; Bools to BoolV
-    ['true (BoolV #t)]
-    ['false (BoolV #f)]
-    
-    ;; Symbols to IdC
     [(? symbol? sym) (IdC (valid-id? sym))]
-    
-    ;; Function applications
-    [(list (? symbol? f) a ...)
-     (match f
-       ['lambda
-        (match a
-          [(list args body)
-           (AppC (ClosV args (parse body) '()) (map parse args))]
-          [other (error 'parse "[AAQZ] invalid lambda syntax: ~e" other)])]
-       [else (AppC (IdC (valid-id? f)) (map parse a))])]
-    
-    ;; Error handling for unrecognized forms
+    [(? string? str) (StringV str)]
+    [(list 'if test then else) (IfC (parse test) (parse then) (parse else))]
+    [(list 'bind (list (? symbol? a) '= b) ... c) (AppC (LamC (check-args (cast a (Listof Symbol)))
+                                                              (parse c))
+                                                        (map parse (cast b (Listof Sexp))))]
+    [(list (list (? symbol? a) ...) '=> b) (LamC (check-args (cast a (Listof Symbol)))
+                                                 (parse b))]
+    [(list f a ...) (AppC (parse f) (map parse a))]
     [other (error 'parse "[AAQZ] syntax error: ~e" other)]))
 
-; my take on parse
 
-(define (bind [n : Symbol] [v : Value]) : Binding
-  (Binding n v))
+(define (interp [expr : ExprC] [env : Env]) : Value
+  (match expr
+    [(NumV n) (NumV n)]
+    [(IdC s) (lookup s env)]
+    [(LamC a b) (ClosV a b env)]
+    [(IfC test then else) (match (interp test env)
+                            [(? BoolV? t) (if (equal? t (BoolV #t))
+                                              (interp then env)
+                                              (interp else env))]
+                            [other (error 'interp "[AAQZ] non-Boolean test in if statement: ~e"
+                                           (serialize other))])]
+    [(AppC f a) (match (interp f env)
+                  [(ClosV args body cenv)
+                   (if (equal? (length args) (length a))
+                       (interp body
+                               (foldl (lambda ([param : Symbol] [arg : ExprC] [acc-env : Env])
+                                        (extend-env (bind param (interp arg env)) acc-env))
+                                      cenv args a))
+                       (error 'interp "[AAQZ] wrong arity: ~e" f))]
+                  [(PrimV op) (handle-prims op (map (lambda ([e : ExprC]) (interp e env))
+                                                    a))]
+                  [other (error 'interp "[AAQZ] application of a non-closure: ~e"
+                                (serialize other))])]))
+
+(define (handle-prims [op : Symbol] [args : (Listof Value)]) : Value
+  (match (cons op args)
+    [(cons 'true '()) (BoolV #t)]
+    [(cons 'false '()) (BoolV #f)]
+    [(cons 'error (? StringV? s))
+     (error 'interp "[AAQZ] user-error: ~e" (serialize s))]
+    [(cons 'equal? (list x y))
+     (match (cons x y)
+       [(cons (or (? PrimV?) (? ClosV?))
+              (or (? PrimV?) (? ClosV?)))
+        (BoolV #f)]
+       [other (BoolV (equal? x y))])]
+    [(cons arith (list x y))
+     (match (cons x y)
+       [(cons (NumV x) (NumV y))
+        (match arith
+          ['+ (NumV (+ x y))]
+          ['- (NumV (- x y))]
+          ['* (NumV (* x y))]
+          ['/ (NumV (if (positive? y)
+                        (/ x y)
+                        (error 'interp "[AAQZ] division by zero")))]
+          ['<= (BoolV (<= x y))])]
+       [other (error 'interp "[AAQZ] arithmetic operation with non-number: ~e" arith)])]
+    [other (error 'interp "[AAQZ] wrong arity for operation: ~e" op)]))
 
 (define (serialize [val : Value]) : String
   (match val
@@ -119,48 +128,30 @@
     [(? ClosV?) "#<procedure>"]
     [(? PrimV?) "#<primop>"]))
 
-
-(define (interp [expr : ExprC] [env : Env]) : Value
-  (match expr
-    [(NumV n) (NumV n)]
-    [(IdC s) (lookup s env)]
-    [(LamC a b) (ClosV a b env)]
-    [(AppC f a) (define f-val (interp f env))
-                (match f-val
-                  [(ClosV args body env)
-                   (if (equal? (length (ClosV-args f-val)) (length a))
-                       (interp (ClosV-body f-val)
-                               (foldl (lambda ([param : Symbol] [arg : ExprC] [e : Env])
-                                        (extend-env (bind param (interp arg e))
-                                                    e))
-                                      (ClosV-env f-val) (ClosV-args f-val) a))
-                       (error 'interp "[AAQZ] wrong arity: ~e" f))]
-                  [(? PrimV? (BinaryP op))
-                   (op (interp (first a) env) (interp (second a) env))]
-                  [(? PrimV? (UnaryP op))
-                   (op (interp (first a) env))])]))
+(define (bind [n : Symbol] [v : Value]) : Binding
+  (Binding n v))
 
 (define (lookup [for : Symbol] [env : Env]) : Value
   (match env
-    ['() (error 'lookup "[AAQZ] binding not found: ~e" for)]
-    [(cons (Binding name val) r) (if (symbol=? for name) val (lookup for r))]))
+    ['() (error 'lookup "[AAQZ] id out of bounds: ~e" for)]
+    [(cons (Binding name val) r) (if (symbol=? for name)
+                                     val
+                                     (lookup for r))]))
 
-; TODO change to match new specification and add to parse function
-; check if the given symbol is a valid id under the AAQZ language
+; check if the given symbol is a valid id under the AAQZ4 language
 ; if it is, return the symbol, else throw an error
 (define (valid-id? [s : Symbol]) : Symbol
-  (if (member s '(* + - / def ifleq0? =>))
-      (error 'valid-id? "[AAQZ] id ~e not permitted" s)
+  (if (member s '(if => bind =))
+      (error 'valid-id? "[AAQZ] id not permitted: ~e" s)
       s))
 
-; TODO add to interp
-; given a list of args, return the list if all arg Symbols are unique and valid ids
+; given a list of arg symbols, return the list if all symbols are unique and are also valid ids
 ; else, throw an error
 (define (check-args [args : (Listof Symbol)]) : (Listof Symbol)
   (match args
     ['() '()]
     [(cons f r) (if (member (valid-id? f) r)
-                    (error 'parse-fundef "[AAQZ] duplicate argument names: ~e" f)
+                    (error 'parse "[AAQZ] duplicate argument names: ~e" f)
                     (cons f (check-args r)))]))
 
 
@@ -170,3 +161,12 @@
                     (LamC '(a b)
                           (AppC (IdC '+) (list (IdC 'a) (IdC 'b))))
                     (list (NumV 10) (NumV 15))) top-env))
+
+(top-interp '{bind [f = {(x) => {+ x 1}}]
+      [y = 7]
+      {f y}})
+
+(top-interp '{bind [fact = {(self n) => {if {<= n 0}
+                               1
+                               {* n {self self {- n 1}}}}}]
+      {fact fact 4}})
